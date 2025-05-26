9535308d7060368aec45c9307f93eff6a5cc07bd
PabloPulgoso <pablo.pulgoso.1010@gmail.com>
2025-05-26 09:24:45 +0200

    Activer/Desactiver bras

--- Software/Software_Exodus_VSC_Project/src/Hardware_lib/LibDriver.cpp
+++ Software/Software_Exodus_VSC_Project/src/Hardware_lib/LibDriver.cpp
@@ -13,18 +13,38 @@ DriverMotor::DriverMotor(int pinParent1, int pinParent2, ParentDevice* parent)
   DriverMotor::pinParent1 = pinParent1;
   DriverMotor::pinParent2 = pinParent2;
   DriverMotor::parent= parent;
+  DriverMotor::actif = false;
 }
 void DriverMotor::RaiseArm()
 {
+  if (DriverMotor::actif)
+  { 
   parent->setPin(pinParent1,HIGH);
   parent->setPin(pinParent2,LOW);
+  }
+
+  else{
+    return;
+  }
 }
 
 void DriverMotor::LowerArm()
-{
+{  
+  if (DriverMotor::actif)
+  { 
   parent->setPin(pinParent1,LOW);
   parent->setPin(pinParent2,HIGH);
+  }
+
+  else{
+    return;
+  }
 }
+
+void DriverMotor::toggleActive(){
+        DriverMotor::actif = !DriverMotor::actif; 
+}
+
 void DriverMotor::StopArm()
 {
   parent->setPin(pinParent1,LOW);
--- Software/Software_Exodus_VSC_Project/src/Hardware_lib/LibDriver.h
+++ Software/Software_Exodus_VSC_Project/src/Hardware_lib/LibDriver.h
@@ -40,9 +40,15 @@ class DriverMotor {
      */
     void StopArm();
 
+    /**
+     * @brief Change l'état du moteur.
+     */
+    void toggleActive();
+
   private: 
     int pinParent1;      ///< Premier pin de commande du moteur
     int pinParent2;      ///< Second pin de commande du moteur
+    bool actif;          ///< Indicateur si le moteur est actif ou non
     ParentDevice* parent;///< Pointeur vers l'interface de commande (registre ou direct)
 };
 
